54e3b549add625d4504fc67ba6109c66ca769b5e
Angus Lam <angushtlam@gmail.com>
2019-07-02 17:43:23 -0400

    Changed font to use Helvetica

--- src/theme.js
+++ src/theme.js
@@ -1,39 +1,51 @@
-import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
-import responsiveFontSizes from '@material-ui/core/styles/responsiveFontSizes';
-
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
+import responsiveFontSizes from '@material-ui/core/styles/responsiveFontSizes'
 
 const theme = createMuiTheme({
-    typography: {
-        h1: {
-            fontSize: '4rem'
-        },
-        h2: {
-            fontSize: '3.4rem'
-        },
-        h3: {
-            fontSize: '2.8rem'
-        },
-        h4: {
-            fontSize: '2.4rem'
-        },
-        h5: {
-            fontSize: '2rem'
-        },
-        h6: {
-            fontSize: '1.5rem'
-        }
+  typography: {
+    fontFamily: [
+      'Helvetica',
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    h1: {
+      fontSize: '4rem',
     },
-    palette: {
-        primary: {
-            main: '#2cb5e8',
-        },
-        secondary: {
-            main: "#1fc8db",
-        },
-        text: {
-            white: '#FFFFFF'
-        },
+    h2: {
+      fontSize: '3.4rem',
     },
-});
+    h3: {
+      fontSize: '2.8rem',
+    },
+    h4: {
+      fontSize: '2.4rem',
+    },
+    h5: {
+      fontSize: '2rem',
+    },
+    h6: {
+      fontSize: '1.5rem',
+    },
+  },
+  palette: {
+    primary: {
+      main: '#2cb5e8',
+    },
+    secondary: {
+      main: '#1fc8db',
+    },
+    text: {
+      white: '#FFFFFF',
+    },
+  },
+})
 
-export default responsiveFontSizes(theme);
+export default responsiveFontSizes(theme)
